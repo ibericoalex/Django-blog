6947cbb0909a88119c6023064aeced2dab3d0f2a
Iberico Alex <iberico_alex@hotmail.com>
2024-02-11 20:30:14 +0000

    Django administration

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,6 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_d11k=2vnnia_r)vixv=!#g7_m@-9^mpz=7!^3j1gg@#5(a6__'
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,6 +91,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
